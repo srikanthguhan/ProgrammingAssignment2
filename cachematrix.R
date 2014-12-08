b7da68333e7afe5061209e2b548bda97f0fef20b
srikanthguhan <srikanthguhan@gmail.com>
2014-12-08 13:54:41 +0530

    Formatting changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,46 @@
 ## Caching the Inverse of a Matrix
-### Variables: x,y <- matrix objects
-### CacInv <- Cached Inverse
-### CalInv <- Calculated Inverse
+### Variables: x,y: matrix objects
+### CacInv: Cached Inverse
+### CalInv: Calculated Inverse
 
-## (i)          makeCacheMatrix <- Initializes a matrix object
+## (i)          makeCacheMatrix: creates a special "matrix" object 
+##              that can cache its inverse
 
-## (ii)         set <- reinitializes matrix object by
+## (ii)         set: re-initializes matrix object by
 ##              setting a different matrix value and setting CacInv NULL
-## (iii)        get <- Gets the value of matrix object
-## (iv)         setInv <- Sets CalInv as CacInv
-## (v)          getInv <- Gets the current CacInv value
+## (iii)        get: Gets the value of matrix object
+## (iv)         setInv: Sets CalInv as CacInv
+## (v)          getInv: Gets the current CacInv value
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function( x = matrix() ) {
         CacInv <- NULL
-        set <- function(y){
+        set <- function( y ) {
                 x <<- y
                 CacInv <<- NULL
         } 
-        get <- function() {x}
-        setInv <- function(CalInv){
+        get <- function() { x }
+        setInv <- function( CalInv ) {
                 CacInv <<- CalInv
         }
-        getInv <- function() {CacInv}
-        list(set = set, get = get,
-             setInv = setInv,
-             getInv = getInv)
+        getInv <- function() { CacInv }
+        list( set = set , get = get , setInv = setInv , getInv = getInv )
 }
 
-## (vi)         cacheSolve <- Calculates matrix inverse if CacInv is NULL
-##              or returns CacInv
+## (vi)         cacheSolve: Computes the inverse of the special "matrix" 
+##              returned by makeCacheMatrix above. If the inverse has 
+##              already been calculated (and the matrix has not changed),
+##              then the cachesolve will retrieve the inverse from the cache
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function( x = list() ) {
         Inv <- x$getInv()
-        if(!is.null(Inv)) {
+        if( !is.null( Inv ) ) {
                 message("Getting cached Inverse")
-                return(Inv)
-        } ## In case the matrix values are modified, 
-        ## the inverse it set to NULL automatically by the set() function
+                return( Inv )
+        }       ## In case the matrix values are modified, 
+                ## the inverse it set to NULL automatically by the set() function
         mat <- x$get()
-        Inv <- solve(mat)
-        x$setInv(Inv)
+        Inv <- solve( mat )
+        x$setInv( Inv )
         Inv
 }
+## The code was run and the outputs were as expected
